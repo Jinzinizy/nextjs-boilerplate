6748bdf704352c9e864e3af5381512b1cf21caab
Jinzinizy <sin_slare@hotmail.com>
2024-09-16 21:38:50 +0100

    sa

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,11 @@ export default function Home() {
       </div>
 
        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p className="text-center text-white sm:text-left text-lg">
-          Welcome to my submission for the Digital Customer Success Manager role at Vercel,<br />
-          Click on the buttons below to view my answers!<br /><br /><br />
-          Looking forward to hearing feedback from you soon; thanks!
-        </p>
+       <p className="text-center text-white text-lg">
+        Welcome to my submission for the Digital Customer Success Manager role at Vercel,<br/>
+        Click on the buttons below to view my answers!<br /><br />
+        Looking forward to hearing feedback from you soon; thanks!
+      </p>
       </main>
 
       <footer className="row-start-3 flex flex-col items-center gap-4 w-full">
@@ -33,7 +33,7 @@ export default function Home() {
               Question {i + 1}
             </a>
           ))}
-          
+
         </div>
         <a
           className="rounded-full border border-solid border-gray-600 transition-colors flex items-center justify-center hover:bg-gray-800 text-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full max-w-screen-sm"
