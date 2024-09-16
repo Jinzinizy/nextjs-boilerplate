f2c0f86542c005ccf9ba80f3ec892c82af8c9c98
Jinzinizy <sin_slare@hotmail.com>
2024-09-16 23:03:04 +0100

    sda

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </div>
 
        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <p className="text-center text-white text-lg text-3xl">
+       <p className="text-center text-white text-lg text-3c1">
         Welcome to my submission for the Digital Customer Success Manager role at Vercel,<br/>
         Click on the buttons below to view my answers!<br /><br />
         Looking forward to hearing feedback from you soon; thanks =]
