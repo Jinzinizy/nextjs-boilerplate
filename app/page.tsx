0099cc1606c2a63ccef23ab299d51dcd6e969fa3
Jinzinizy <sin_slare@hotmail.com>
2024-09-16 21:17:55 +0100

    d

--- app/page.tsx
+++ app/page.tsx
@@ -14,26 +14,29 @@ export default function Home() {
         />
       </div>
 
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <p className="text-center text-white sm:text-left text-lg">
-          Welcome to my submission for the Digital Customer Success Manager role at Vercel, <br/> 
-          Click on the buttons below view my answers!<br/><br/>
+          Welcome to my submission for the Digital Customer Success Manager role at Vercel,<br /><br />
+          Click on the buttons below to view my answers!<br /><br />
           Looking forward to hearing feedback from you soon; thanks!
         </p>
       </main>
-      
-      <footer className="row-start-3 flex gap-4 flex-wrap items-center justify-center">
-        {Array.from({ length: 9 }, (_, i) => (
-          <a
-            key={i}
-            className="rounded-full border border-solid border-gray-600 transition-colors flex items-center justify-center hover:bg-gray-800 text-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href={`#question${i + 1}`}
-          >
-            Question {i + 1}
-          </a>
-        ))}
-           <a
-          className="rounded-full border border-solid border-gray-600 transition-colors flex items-center justify-center hover:bg-gray-800 text-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+
+      <footer className="row-start-3 flex flex-col items-center gap-4 w-full">
+        <div className="flex gap-4 flex-wrap items-center justify-center">
+          {Array.from({ length: 8 }, (_, i) => (
+            <a
+              key={i}
+              className="rounded-full border border-solid border-gray-600 transition-colors flex items-center justify-center hover:bg-gray-800 text-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              href={`#question${i + 1}`}
+            >
+              Question {i + 1}
+            </a>
+          ))}
+        </div>
+
+        <a
+          className="rounded-full border border-solid border-gray-600 transition-colors flex items-center justify-center hover:bg-gray-800 text-white text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full max-w-screen-sm"
           href="#feedback"
         >
           Feedback
