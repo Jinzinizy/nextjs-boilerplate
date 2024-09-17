29cf196c79d3fbdc7c7cacd59d68b43e0fcb5f31
Jinzinizy <sin_slare@hotmail.com>
2024-09-17 01:35:53 +0100

    hehe

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
           </main>
 
           {/* Footer with Questions and Feedback buttons */}
-          <footer className="absolute bottom-1/4 flex flex-col items-center w-2/3">
+          <footer className="absolute bottom-1/6 flex flex-col items-center w-2/3">
             <div className="flex gap-2 justify-between w-full">
               {/* Buttons for Questions 1-8 */}
               {Array.from({ length: 8 }, (_, i) => (
