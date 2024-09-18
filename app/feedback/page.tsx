d7d3d920b92433656b493bb7fcebf869c287c605
Jinzinizy <sin_slare@hotmail.com>
2024-09-18 19:25:01 +0100

    feedback

---
+++ app/feedback/page.tsx
@@ -0,0 +1,9 @@
+// /app/feedback/page.tsx
+export default function FeedbackPage() {
+  return (
+    <div>
+      <p>Great takehome assignment, I thouroughly enjoyed it. I believe the difficulty was just about right. <br/>
+     The only feedback I would give is that this exercise requires a user to already have insalled their own Terminal, Editor etc... which not every candidate may have on their home setup.</p>
+    </div>
+  );
+}
